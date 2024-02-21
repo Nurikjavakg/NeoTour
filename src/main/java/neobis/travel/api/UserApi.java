8ae28f69936738ea4de3940bb0550818cdb7f7ca
Nurik <nalymbaide@gmail.com>
2024-02-21 20:18:30 +0600

    added @permitAll

--- src/main/java/neobis/travel/api/UserApi.java
+++ src/main/java/neobis/travel/api/UserApi.java
@@ -2,6 +2,7 @@ package neobis.travel.api;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.annotation.security.PermitAll;
 import lombok.RequiredArgsConstructor;
 import neobis.travel.dto.AuthenticationSignInResponse;
 import neobis.travel.dto.AuthenticationSignUpResponse;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/users")
 @Tag(name = "User Api")
 @CrossOrigin(origins = "*", maxAge = 3600)
+@PermitAll
 public class UserApi {
 
     private final UserService userService;
